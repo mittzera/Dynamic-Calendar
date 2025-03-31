86bbf1e29eed94bef2233c5b0b1226d4b9ecfa0f
otavio.silva <otavioaraujo490@gmail.com>
2025-03-31 09:16:25 -0300

    feat: add IEvent interface for event data structure

---
+++ interfaces/Event.ts
@@ -0,0 +1,22 @@
+export interface IEvent {
+  _id: string;
+  id: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  dayOfTheWeek: string;
+  title: string;
+  description: string;
+  createdBy: {
+    username: string;
+    _id: string;
+    picture: string;
+  };
+  createdAt: string;
+  updatedAt: string;
+  updatedBy?: {
+    username: string;
+    _id: string;
+    picture: string;
+  };
+}
